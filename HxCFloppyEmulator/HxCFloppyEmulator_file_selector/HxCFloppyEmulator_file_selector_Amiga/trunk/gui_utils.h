0260b48acb1f9041c76f8e030b31c200d6896a63
jfdelnero <jfdelnero@63fa2f70-6a9b-4bc4-b855-9c6a949b1d69>
2015-05-03 10:50:35 +0000

    CRC Generation added for the new STM32 firmware.

--- HxCFloppyEmulator/HxCFloppyEmulator_file_selector/HxCFloppyEmulator_file_selector_Amiga/trunk/gui_utils.h
+++ HxCFloppyEmulator/HxCFloppyEmulator_file_selector/HxCFloppyEmulator_file_selector_Amiga/trunk/gui_utils.h
@@ -10,5 +10,5 @@ void box(unsigned short x_p1,unsigned short y_p1,unsigned short x_p2,unsigned sh
 int init_display();
 void set_color_scheme(unsigned char color);
 
-#define VERSIONCODE "1.11a"
-#define DATECODE "04/12/2015"
+#define VERSIONCODE "1.12a"
+#define DATECODE "05/03/2015"
