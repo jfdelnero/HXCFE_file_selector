bba759c3be665c29c9be37b3755643e017474a35
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2016-11-22 21:33:13 +0100

    SDL keymap update.

--- sdl/keymap.h
+++ sdl/keymap.h
@@ -1,26 +1,50 @@
 
 keyboard_funct_mapper keysmap[]=
 {
-	{FCT_UP_KEY,0x4C},
-	{FCT_DOWN_KEY,0x4D},
-	{FCT_LEFT_KEY,0x4F},
-	{FCT_RIGHT_KEY,0x4E},
-	{FCT_SELECT_FILE_DRIVEA,0x44},
-	{FCT_SAVE,0x58},
-	{FCT_REBOOT,0x57},
-	{FCT_CLEARSLOT,0x41},
-	{FCT_HELP,0x5F},
-	{FCT_SAVEREBOOT,0x59},
-	{FCT_SELECTSAVEREBOOT,0x56},
-	{FCT_OK,0x40},
-	{FCT_SHOWSLOTS,0x42},
-	{FCT_SEARCH,0x50},
-	{FCT_TOP,0x61},
-	{FCT_CHGCOLOR,0x51},
-	{FCT_EMUCFG,0x52},
-	{FCT_ESCAPE,0x45},
-	{FCT_NO_FUNCTION,0x00}
+	{FCT_UP_KEY,             0x00},
+	{FCT_DOWN_KEY,           0x01},
+	{FCT_LEFT_KEY,           0x02},
+	{FCT_RIGHT_KEY,          0x03},
+	{FCT_SELECT_FILE_DRIVEA, 0x04},
+	{FCT_SAVE,               0x05},
+	{FCT_REBOOT,             0x06},
+	{FCT_CLEARSLOT,          0x07},
+	{FCT_HELP,               0x08},
+	{FCT_SAVEREBOOT,         0x09},
+	{FCT_SELECTSAVEREBOOT,   0x0A},
+	{FCT_OK,                 0x0B},
+	{FCT_SHOWSLOTS,          0x0C},
+	{FCT_SEARCH,             0x0D},
+	{FCT_TOP,                0x0E},
+	{FCT_CHGCOLOR,           0x0F},
+	{FCT_EMUCFG,             0x10},
+	{FCT_ESCAPE,             0x11},
+	{FCT_NO_FUNCTION,        0x00}
 };
+
+unsigned long sdl_scancode[]=
+{
+	SDLK_UP,
+	SDLK_DOWN,
+	SDLK_LEFT,
+	SDLK_RIGHT,
+	SDLK_RETURN,
+	SDLK_F9,
+	SDLK_F8,
+	SDLK_BACKSPACE,
+	SDLK_F11,
+	SDLK_F10,
+	SDLK_F7,
+	SDLK_RETURN,
+	SDLK_TAB,
+	SDLK_F1,
+	SDLK_RSHIFT,
+	SDLK_F2,
+	SDLK_F3,
+	SDLK_ESCAPE,
+	0x00
+};
+
 
 keyboard_funct_mapper char_keysmap[]=
 {
