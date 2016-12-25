62804ab42b6cbfe8fa4cffa6f799d6998988e592
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2016-12-25 16:19:04 +0100

    Missing definitions added.

--- sdl/hardware.h
+++ sdl/hardware.h
@@ -2,9 +2,12 @@ void init_fdc(unsigned char drive);
 int jumptotrack(unsigned char t);
 unsigned char readsector(unsigned char sectornum,unsigned char * data,unsigned char invalidate_cache);
 unsigned char writesector(unsigned char sectornum,unsigned char * data);
+int get_start_unit(char * path);
 
 unsigned char Keyboard();
 unsigned char wait_function_key();
+unsigned char get_char();
+void flush_char();
 
 void reboot();
 
@@ -13,9 +16,33 @@ unsigned short get_vid_mode();
 void setvideomode(int mode);
 void disablemousepointer();
 
+
+#ifndef BMAPTYPEDEF
+#define BMAPTYPEDEF
+
+typedef  struct _bmaptype
+{
+   int type;
+   int Xsize;
+   int Ysize;
+   int size;
+   int csize;
+   unsigned char * data;
+}bmaptype;
+
+#endif
+
+void display_sprite(unsigned char * membuffer, bmaptype * sprite,unsigned short x, unsigned short y);
+void print_char8x8(unsigned char * membuffer, bmaptype * font,unsigned short x, unsigned short y,unsigned char c);
+
 void init_timer();
 
 #define L_INDIAN(var) (var)
 #define B_INDIAN(var) (((var&0x000000FF)<<24) |((var&0x0000FF00)<<8) |((var&0x00FF0000)>>8) |((var&0xFF000000)>>24))
 
 int process_command_line(int argc, char* argv[]);
+
+#ifdef WIN32
+void sleep(int secs);
+#endif
+void waitms(int ms);
