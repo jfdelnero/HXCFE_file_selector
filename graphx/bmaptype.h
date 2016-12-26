6f31f39295863147dc395d5acd1d0ccc20b03616
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2016-12-26 17:10:48 +0100

    bitmap type moved.

---
+++ graphx/bmaptype.h
@@ -0,0 +1,14 @@
+#ifndef BMAPTYPEDEF
+#define BMAPTYPEDEF
+
+typedef  struct _bmaptype
+{
+   int type;
+   int Xsize;
+   int Ysize;
+   int size;
+   int csize;
+   unsigned char * data;
+}bmaptype;
+
+#endif
