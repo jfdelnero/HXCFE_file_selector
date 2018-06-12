c9d638fd780e6741f565ca372a692030bbd909a7
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2018-06-12 21:41:00 +0200

    Better output. Preparing universal font layout support.

--- graphx/build_font/main.c
+++ graphx/build_font/main.c
@@ -1,6 +1,35 @@
+/*
+//
+// Copyright (C) 2017-2018 Jean-François DEL NERO
+//
+// This file is part of the FontCP&P software
+//
+// FontCP&P may be used and distributed without restriction provided
+// that this copyright statement is not removed from the file and that any
+// derivative work contains the original copyright notice and the associated
+// disclaimer.
+//
+// FontCP&P is free software; you can redistribute it
+// and/or modify  it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// FontCP&P is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//   See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with FontCP&P; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include <stdint.h>
 
 #include "bmp_file.h"
 
@@ -13,18 +42,45 @@
 #define FONT_MATRIX_X_SIZE 16
 #define FONT_MATRIX_Y_SIZE 16
 
+typedef struct font_spec_
+{
+	unsigned int nb_of_chars;
+
+	unsigned int char_x_size;
+	unsigned int char_y_size;
+
+	unsigned int x_char_step;
+	unsigned int y_char_step;
+
+	unsigned int chars_per_line;
+	unsigned int chars_per_row;
+
+	unsigned int x_offset;
+	unsigned int y_offset;
+}font_spec;
+
 unsigned char framebuffer[(128*64) / 8];
 
 void printbuf(unsigned char * buf,int size)
 {
 	int i;
-	printf("\n");
+
 	for(i=0;i<size;i++)
 	{
 		if(!(i&0xF))
-			printf("\n");
-
-		printf("0x%.2X,",buf[i]);
+		{
+			if(i)
+				printf("\n\t");
+			else
+				printf("\x7B\n\t");
+		}
+		printf("0x%.2X",buf[i]);
+		if( i < size - 1 )
+			printf(",");
+		else
+		{
+			printf("\n\x7D\x3B\n");
+		}
 	}
 	printf("\n");
 }
@@ -40,7 +96,7 @@ int getpixstate(bitmap_data * bdata, int xpos, int ypos)
 	}
 	else
 	{
-		printf("outside bmp ! : %d x %d\n",xpos,ypos );
+		printf("getpixstate : outside bmp ! : %d x %d\n", xpos, ypos );
 	}
 	return 0;
 }
@@ -72,25 +128,34 @@ void set_ssd1306_pix(unsigned char * buf,int x,int y,int xsize,int ysize)
 	int bitoffset;
 	int page,bit,seg;
 
-	page = y / 8;
-	bit = y & 7;
-	seg = x;
+	if( (x < xsize) && (y < ysize) )
+	{
+		page = y / 8;
+		bit = y & 7;
+		seg = x;
 
-	bitoffset = (xsize * 8 * page) + (seg * 8) + bit;
+		bitoffset = (xsize * 8 * page) + (seg * 8) + bit;
 
-	buf[bitoffset/8] |= (0x01<< (bitoffset&7));
-
+		buf[bitoffset/8] |= (0x01<< (bitoffset&7));
+	}
 }
 
 void set_char_pix(unsigned char * buf,int x,int y,int xsize,int ysize)
 {
 	int bitoffset;
 
-	bitoffset = (xsize * y) + x;
+	if( (x < xsize) && (y < ysize) )
+	{
+		bitoffset = (xsize * y) + x;
 
-	buf[bitoffset/8] |= (0x80 >> (bitoffset&7));
-
+		buf[bitoffset/8] |= (0x80 >> (bitoffset&7));
+	}
+	else
+	{
+		printf("set_char_pix : outside bitmap area ! : %d x %d\n", x, y );
+	}
 }
+
 unsigned char * extractchar(bitmap_data * bdata,int x,int y,int xsize,int ysize, int * bufsize)
 {
 	unsigned char * charbuf;
@@ -131,42 +196,70 @@ int main(int argc, char *argv[])
 {
 	int bufsize,xchar,ychar,bufoffset;
 	bitmap_data bdata;
-	unsigned char * charbuf;
+	unsigned char * char_sprite;
 	unsigned char * finalfontbuffer;
+	font_spec ifnt;
 
-	printf("build font\n");
+	printf("FontCP&P v0.8\n");
+	printf("(c) Jean-François DEL NERO / HxC2001 2017-2018\n\n");
 
 	memset(&framebuffer,0,sizeof(framebuffer));
 
 	if(argc > 1)
 	{
+		// Default font : 256 chars 8x8
+		ifnt.nb_of_chars = 256;
+		ifnt.char_x_size = FONT_X_SIZE;
+		ifnt.char_y_size = FONT_Y_SIZE;
+		ifnt.x_char_step = FONT_X_SRCSTEP_SIZE;
+		ifnt.y_char_step = FONT_Y_SRCSTEP_SIZE;
+		ifnt.x_offset = 0;
+		ifnt.y_offset = 0;
+		ifnt.chars_per_line = FONT_MATRIX_X_SIZE;
+		ifnt.chars_per_row = FONT_MATRIX_Y_SIZE;
+
 		if(!bmp_load(argv[1],&bdata))
 		{
+
 			printf("Bmp Loaded... Xsize: %d, Ysize: %d\n",bdata.xsize,bdata.ysize);
 
-			charbuf = extractchar(&bdata,0,0,FONT_X_SIZE,FONT_Y_SIZE,&bufsize);
-			free(charbuf);
-			printf("char size : %d total table size : %d\n",bufsize,256 * bufsize);
+			// Get the sprite size.
+			char_sprite = extractchar(&bdata,0,0,ifnt.char_x_size,ifnt.char_y_size,&bufsize);
 
-			finalfontbuffer = malloc(256 * bufsize);
+			free(char_sprite);
+
+			printf("Char sprite size : %d total table size : %d\n",bufsize,ifnt.nb_of_chars * bufsize);
+
+			finalfontbuffer = malloc(ifnt.nb_of_chars * bufsize);
 			if(finalfontbuffer)
 			{
-				memset(finalfontbuffer,0,256 * bufsize);
+				memset(finalfontbuffer,0,ifnt.nb_of_chars * bufsize);
 				bufoffset=0;
-				for(ychar = 0; ychar < FONT_MATRIX_Y_SIZE; ychar++)
+
+				for(ychar = 0; ychar < ifnt.chars_per_row; ychar++ )
 				{
-					for(xchar = 0; xchar < FONT_MATRIX_X_SIZE ; xchar++)
+					for(xchar = 0; xchar < ifnt.chars_per_line ; xchar++)
 					{
-						charbuf = extractchar(&bdata,xchar*FONT_X_SRCSTEP_SIZE,ychar*FONT_Y_SRCSTEP_SIZE,FONT_X_SIZE,FONT_Y_SIZE,&bufsize);
-						printf("Char %d, size:%d, x:%d, y:%d, offset:%d\n",(ychar * FONT_MATRIX_X_SIZE)+xchar,bufsize,xchar*FONT_X_SRCSTEP_SIZE,ychar*FONT_Y_SRCSTEP_SIZE,bufoffset);
-						memcpy( finalfontbuffer+bufoffset, charbuf, bufsize);
+						char_sprite = extractchar(&bdata, xchar*ifnt.x_char_step, ychar*ifnt.y_char_step, ifnt.char_x_size, ifnt.char_y_size, &bufsize);
+
+						printf("Char %d, size:%d, x:%d, y:%d, offset:%d\n", \
+								(ychar * ifnt.chars_per_line) + xchar, \
+								bufsize,
+								xchar * ifnt.x_char_step,
+								ychar * ifnt.y_char_step,
+								bufoffset);
+
+						memcpy( finalfontbuffer+bufoffset, char_sprite, bufsize);
+
 						bufoffset += bufsize;
-						free(charbuf);
+
+						free(char_sprite);
 					}
 				}
 			}
 
-			printbuf((unsigned char*)finalfontbuffer,256 * bufsize);
+			printf("\nconst unsigned char font[] =\n");
+			printbuf( (unsigned char*)finalfontbuffer, ifnt.nb_of_chars * bufsize);
 
 			free(finalfontbuffer);
 		}
