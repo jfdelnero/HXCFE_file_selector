dec5a08221f6520ff99bf9c05eba935033508df0
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2018-06-16 21:13:45 +0200

    generated fonts.

---
+++ graphx/fonts/font_st_8x8.h
@@ -0,0 +1,19 @@
+////////////////////////////////////
+//  FontCP&P generated font data  //
+////////////////////////////////////
+
+#ifndef FONT_HEADER
+#define FONT_HEADER
+typedef struct _font_type
+{
+	unsigned int nb_of_chars;
+	unsigned int char_x_size;
+	unsigned int char_y_size;
+	unsigned int char_size;
+	unsigned int buffer_size;
+	const unsigned char * font_data;
+}font_type;
+#endif
+
+extern const unsigned char data_font_st_8x8[];
+extern font_type font_st_8x8;
