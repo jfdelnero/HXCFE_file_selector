475d838174193b19b4d3bd8e8d66dab0cbdf5eae
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2018-06-17 22:21:33 +0200

    Texts/pages layout changed to fit the screen with taller fonts.

--- msg_txt.c
+++ msg_txt.c
@@ -1,3 +1,30 @@
+/*
+//
+// Copyright (C) 2009-2018 Jean-François DEL NERO
+//
+// This file is part of the HxCFloppyEmulator file selector.
+//
+// HxCFloppyEmulator file selector may be used and distributed without restriction
+// provided that this copyright statement is not removed from the file and that any
+// derivative work contains the original copyright notice and the associated
+// disclaimer.
+//
+// HxCFloppyEmulator file selector is free software; you can redistribute it
+// and/or modify  it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// HxCFloppyEmulator file selector is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//   See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with HxCFloppyEmulator file selector; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
@@ -18,17 +45,13 @@ const char startup_msg[]=
 	"\n"
 	"Email : hxc2001@free.fr\n"
 	"\n"
-	"\n"
 	">>> Press HELP key for the function key list <<<"
 	"\n"
-	"\n"
-	"Check for updates on :\n"
-	"http://hxc2001.free.fr/floppy_drive_emulator/\n"
-	"http://hxc2001.com\n"
-	"\n"
-	"Forum : http://torlus.com/floppy/forum\n"
-	"\n"
-	"Source code on http://github.com/jfdelnero\n"
+	"Updates:  http://hxc2001.free.fr/floppy_drive_emulator/\n"
+	"          http://hxc2001.com                           \n"
+	"Forum:    http://torlus.com/floppy/forum               \n"
+	"Facebook: https://www.facebook.com/groups/hxc2001      \n"
+	"Sources : http://github.com/jfdelnero                  \n"
 	"\n"
 	"This software uses the FAT16/32 File IO Library v2.6\n"
 	"(c) 2003-2013 Ultra-Embedded.com\n"
@@ -36,19 +59,26 @@ const char startup_msg[]=
 
 const char help_scr1_msg[]=
 {
-	"Function Keys (1/2):\n"
+	"Function Keys (1/3):\n"
 	"\n"
 	"-> Into the files browser :\n"
 	"\n"
-	"Keyboard Arrows and Joystick : Browse the SD/USB files\n"
-	"Right Shift                  : Go back to the top of the folder\n"
-	"F7                           : Insert the selected file in the slot 1\n"
-    "                               and restart the computer with this disk.\n"
-	"ENTER/Joystick Fire          : Enter a subfolder/ Select an image\n"
-	"                               and enter the slots selection\n"
+	"Keyboard Arrows and Joystick  : Browse the SD/USB files\n"
+	"Right Shift                   : Go back to the top of the folder\n"
+	"F7                            : Insert the selected file in the slot 1\n"
+    "                                and restart the computer with this disk.\n"
+	"ENTER/Joystick Fire           : Enter a subfolder/ Select an image\n"
+	"                                and enter the slots selection\n"
 	"\n"
 	"\n"
-	"->Into the slots browser :\n"
+	"                   ---Press Enter / Fire to continue---"
+};
+
+const char help_scr2_msg[]=
+{
+	"Function Keys (2/3):\n"
+	"\n"
+	"-> Into the slots browser :\n"
 	"\n"
 	"Keyboard Arrows and Joystick  : Browse the slots selection\n"
 	"BACKSPACE                     : Clear the current slot\n"
@@ -57,12 +87,13 @@ const char help_scr1_msg[]=
 	"ESCAPE                        : Leave the slots browser\n"
 	"\n"
 	"\n"
+	"\n"
 	"                   ---Press Enter / Fire to continue---"
 };
 
-const char help_scr2_msg[]=
+const char help_scr3_msg[]=
 {
-	"Function Keys (2/2):\n"
+	"Function Keys (3/3):\n"
 	"\n"
 	"F1                : Search files in the current folder\n"
 	"                    Type the word to search, then enter\n"
@@ -73,20 +104,29 @@ const char help_scr2_msg[]=
 	"F9                : Save\n"
 	"F10               : Save and Reboot\n"
 	"\n"
+	"\n"
+	"                   ---Press Enter / Fire to continue---"
+};
+
+const char help_scr4_msg[]=
+{
+	"Notes :\n"
+	"\n"
 	"The joystick can be used to browse all the interface and select the images.\n"
 	"The keyboard is not required.\n"
+	"\n"
 	"The first line of each page allows to switch to the next one :\n"
-	"Media files list-> Drive A slots list-> Drive B slots list-> Save/Settings page\n"
+	"Media files list ->Drive A slots list ->Drive B slots list ->Save/Settings page\n"
+	"\n"
+	"\n"
+	"\n"
 	"\n"
 	"\n"
 	"                   ---Press Enter / Fire to continue---"
-
 };
 
-const char help_scr3_msg[]=
+const char help_scr5_msg[]=
 {
-	"\n"
-	"\n"
 	"HxC Floppy Emulator file selector\n"
 	"V" VERSIONCODE " - " DATECODE "\n"
 	"\n"
@@ -95,16 +135,26 @@ const char help_scr3_msg[]=
 	"\n"
 	"Atari ST version optimized by Gilles Bouthenot\n"
 	"\n"
-	"Check for updates on :\n"
-	"http://hxc2001.free.fr/floppy_drive_emulator/\n"
-	"http://hxc2001.com\n"
-	"\n"
-	"Forum : http://torlus.com/floppy/forum\n"
-	"Sources on http://github.com/jfdelnero\n"
-	"\n"
-	"This software uses the\n"
-	"FAT16/32 File IO Library v2.6\n"
+	"This software uses the FAT16/32 File IO Library\n"
 	"(c) 2003-2013 Ultra-Embedded.com\n"
+	"\n"
+	"\n"
+	"---Press Enter / Fire to continue---"
+};
+
+const char help_scr6_msg[]=
+{
+	"HxC Floppy Emulator file selector\n"
+	"V" VERSIONCODE " - " DATECODE "\n"
+	"\n"
+	"(c) 2006-2018 HxC2001 / Jean-Francois DEL NERO\n"
+	"Email : hxc2001@free.fr\n"
+	"\n"
+	"Updates : http://hxc2001.free.fr/floppy_drive_emulator/\n"
+	"          http://hxc2001.com                           \n"
+	"Forum   : http://torlus.com/floppy/forum               \n"
+	"Facebook: https://www.facebook.com/groups/hxc2001      \n"
+	"Sources : http://github.com/jfdelnero                  \n"
 	"\n"
 	"---Press Enter / Fire to continue---"
 };
@@ -113,7 +163,10 @@ const pagedesc help_pages[]=
 {
 	{help_scr1_msg,LEFT_ALIGNED},
 	{help_scr2_msg,LEFT_ALIGNED},
-	{help_scr3_msg,CENTER_ALIGNED},
+	{help_scr3_msg,LEFT_ALIGNED},
+	{help_scr4_msg,LEFT_ALIGNED},
+	{help_scr5_msg,CENTER_ALIGNED},
+	{help_scr6_msg,CENTER_ALIGNED},
 	{0,0}
 };
 
